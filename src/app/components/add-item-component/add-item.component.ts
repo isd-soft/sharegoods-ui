2ca856ed10de89a75c6215999a6b16e4653710d2
oxanadunav <oxana.dunav@gmail.com>
2018-10-16 17:51:12 +0300

    create item(title and description) via formdata

--- src/app/components/add-item-component/add-item.component.ts
+++ src/app/components/add-item-component/add-item.component.ts
@@ -12,6 +12,8 @@ export class AddItemComponent implements OnInit {
 
   item: Item = new Item();
   userId : string;
+  uploadedFiles: File[] = [];
+  formData: FormData = new FormData();
 
   constructor(private router: Router, private itemService: ItemService, private route: ActivatedRoute) { }
 
@@ -21,8 +23,16 @@ export class AddItemComponent implements OnInit {
     });
   }
 
+  onFileChange(event) {
+    this.uploadedFiles.push(event.srcElement.files[0]);
+  }
+
   createItem(): void {
-    this.itemService.createItem(this.userId, this.item)
+    this.formData.append('title', this.item.title);
+    this.formData.append('description', this.item.description);
+
+    //TODO add photos to formData name: file, multipartfile(name and byte[])
+    this.itemService.createItem(this.userId, this.formData)
       .subscribe(data => {
           console.log(data);
         },
