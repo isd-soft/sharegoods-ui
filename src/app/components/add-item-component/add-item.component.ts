b594ee839b2638b2e0bd49a1a5f611b9307c5997
oxanadunav <oxana.dunav@gmail.com>
2018-10-17 11:55:59 +0300

    remove commented code

--- src/app/components/add-item-component/add-item.component.ts
+++ src/app/components/add-item-component/add-item.component.ts
@@ -3,7 +3,6 @@ import {Item} from "../../models/item";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ItemService} from "../../services/item-service/item.service";
 import {ImageUploadComponent} from "angular2-image-upload";
-import {FileHolder} from "angular2-image-upload/src/image-upload/image-upload.component";
 
 @Component({
   selector: 'app-add-item',
@@ -16,12 +15,7 @@ export class AddItemComponent implements OnInit {
   item: Item = new Item();
   userId : string;
   formData: FormData = new FormData();
-  uploadedImages: FileHolder[] = [];
-
-  // fileList: FileList;
-  // uploadedImages: File[] = [];
-  // urls = new Array<string>();
-  itemCreated: boolean = false;
+  uploadedImages: any;
 
   constructor(private router: Router, private itemService: ItemService, private route: ActivatedRoute) { }
 
@@ -31,40 +25,18 @@ export class AddItemComponent implements OnInit {
     });
   }
 
-  // onFileChange(event) {
-  //   this.uploadedImages = [];
-  //   this.urls = [];
-  //   if (event.target.files.length === 0) {
-  //     return;
-  //   }
-  //   this.fileList = event.target.files;
-  //   for (let i = 0; i < this.fileList.length; i++) {
-  //     let reader = new FileReader();
-  //     const file = this.fileList[i];
-  //     reader.onload = (e: any) => {
-  //       this.urls.push(e.target.result);
-  //     };
-  //     if (!file.type.match('image')) {
-  //       continue;
-  //     }
-  //     reader.readAsDataURL(file);
-  //     this.uploadedImages.push(file);
-  //   }
-  // }
-
   createItem(): void {
+    this.uploadedImages = this.imageUploadComponent.files;
     this.formData.append('title', this.item.title);
     this.formData.append('description', this.item.description);
-    this.uploadedImages = this.imageUploadComponent.files;
-    console.log("Uploaded images", this.uploadedImages);
-    for(let i= 0; i < this.uploadedImages.length; i++){
+    for(let i = 0; i < this.uploadedImages.length; i++){
       this.formData.append('file', this.uploadedImages[i].file);
     }
-    console.log("Form data:", this.formData);
+
     this.itemService.createItem(this.userId, this.formData)
       .subscribe(data => {
           console.log(data);
-          this.itemCreated = true;
+          alert("Item was created");
         },
         err => {
           console.log("Error occured to create new item");
