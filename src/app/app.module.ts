1e100a0517a34ac1a8a4e223b615e6faef845fd6
alex <alex.c.turbo@gmail.com>
2018-10-18 15:15:29 +0300

    Fixed rebasing crap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from "@angular/common/http";
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,56 +8,32 @@ import { Config } from './config'
 
 import { UserComponent } from './components/user-component/user.component';
 import { ItemComponent } from './components/item-component/item.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { RegisterComponent } from './components/register-component/register.component';
 
-import { HttpClientModule } from "@angular/common/http";
 import { AddItemComponent } from './components/add-item-component/add-item.component';
 import { FormsModule } from "@angular/forms";
 import {ImageUploadModule} from "angular2-image-upload";
 
-
-=======
-import { LoginComponent } from './components/login-component/login.component';
-
 import { TokenInterceptor } from './auth/token.interceptor';
 import { AuthService } from './auth/auth.service';
->>>>>>> Updates to login component and authorization logic
-=======
 import { LoginComponent } from './components/login-component/login.component';
 
-import { TokenInterceptor } from './auth/token.interceptor';
-import { AuthService } from './auth/auth.service';
->>>>>>> Updates to login component and authorization logic
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     ItemComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     RegisterComponent,
-    AddItemComponent
-=======
+    AddItemComponent,
     LoginComponent
->>>>>>> Updates to login component and authorization logic
-=======
-    LoginComponent
->>>>>>> Updates to login component and authorization logic
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
     ImageUploadModule.forRoot(),
-=======
-=======
->>>>>>> Updates to login component and authorization logic
     FormsModule
   ],
   providers: [
@@ -69,10 +44,6 @@ import { AuthService } from './auth/auth.service';
       useClass: TokenInterceptor,
       multi: true
     }
-<<<<<<< HEAD
->>>>>>> Updates to login component and authorization logic
-=======
->>>>>>> Updates to login component and authorization logic
   ],
   bootstrap: [AppComponent]
 })
