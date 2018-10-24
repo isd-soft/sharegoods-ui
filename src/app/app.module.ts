85777314474d702a9c785fbab5d2c47789c8c2ff
Ion HAREA <vaniusha.harea.1997@mail.ru>
2018-10-24 14:30:02 +0300

    Done a lil bit of comments and refactored some code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ImageUploadModule } from 'angular2-image-upload';
+import { LightboxModule } from 'ngx-lightbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Config } from './config'
-
 import { UserComponent } from '@components/user-list-component/user.component';
 import { ItemComponent } from '@components/item-list-component/item.component';
 import { RegisterComponent } from '@components/register-component/register.component';
@@ -17,11 +16,11 @@ import { FooterComponent } from '@core/components/footer/footer.component';
 import { HeaderComponent } from '@core/components/header/header.component';
 import { LayoutComponent } from '@core/components/layout/layout.component';
 import { NavComponent } from '@core/components/nav/nav.component';
-import { ItemDetailsComponent } from "./components/item-details-component/item-details.component";
-import { LightboxModule } from "ngx-lightbox";
+import { ItemDetailsComponent } from '@components/item-details-component/item-details.component';
 import { TokenInterceptor } from './auth/token.interceptor';
 import { AuthService } from './auth/auth.service';
-import { LoginComponent } from './components/login-component/login.component';
+import { LoginComponent } from '@components/login-component/login.component';
+import { AddCommentComponent } from '@components/add-comment-component/add-comment.component';
 
 
 @NgModule({
@@ -37,7 +36,8 @@ import { LoginComponent } from './components/login-component/login.component';
     LayoutComponent,
     NavComponent,
     HomePageComponent,
-    LoginComponent
+    LoginComponent,
+    AddCommentComponent
   ],
   imports: [
     BrowserModule,
@@ -56,7 +56,6 @@ import { LoginComponent } from './components/login-component/login.component';
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
-    Config,
     AuthService,
     {
       provide: HTTP_INTERCEPTORS,
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Observable, of, from } from 'rxjs';
-import { Config } from 'app/config';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
 import { User } from 'app/models/user';
+import { environment } from '@env/environment';
 
 @Injectable()
 export class AuthService {
 
-  constructor (
-    private config : Config,
-    private http: HttpClient
-  ) {
-
+  constructor(private http: HttpClient) {
   }
 
   public isAuthenticated(): boolean {
     const token = this.getToken();
-    if (token) return true;
-    return false;
+    return !!token;
   }
 
-  public setToken (email:string, password:string) {
+  public setToken(email: string, password: string) {
     sessionStorage.setItem('token', btoa(email + ':' + password));
   }
 
@@ -28,16 +23,16 @@ export class AuthService {
     return sessionStorage.getItem('token');
   }
 
-  public setCurrentUser (user : User) {
+  public setCurrentUser(user: User) {
     sessionStorage.setItem('user', JSON.stringify(user));
   }
 
   public getCurrentUser(): User {
-    let user : User = JSON.parse(sessionStorage.getItem('user'));
+    const user: User = JSON.parse(sessionStorage.getItem('user'));
     return user;
   }
 
-  public removeToken () {
+  public removeToken() {
     sessionStorage.removeItem('token');
   }
 
@@ -45,11 +40,11 @@ export class AuthService {
     sessionStorage.removeItem('user');
   }
 
-  public isLoginDataValid (email, password) {
-    let url = this.config.serverUrl + '/users/login';
-    let header = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
-    let body = new HttpParams().set('email', email).set('password', password);
+  public isLoginDataValid(email, password) {
+    const url = environment.apiUrl + '/users/login';
+    const header = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+    const body = new HttpParams().set('email', email).set('password', password);
 
-    return this.http.post<User>(url, body, { headers: header });
+    return this.http.post<User>(url, body, {headers: header});
   }
 }
