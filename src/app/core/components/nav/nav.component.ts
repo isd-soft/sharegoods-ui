2ed1c80ed477b15865e114f55babd600b19c5724
oxanadunav <oxana.dunav@gmail.com>
2018-10-24 17:25:36 +0300

    add menu for user

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/auth/auth.service';
 import { NavBarLink } from '@models/navbar-link';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -18,13 +19,18 @@ export class NavComponent implements OnInit {
     {
       name: 'Posts',
       url: '/items'
-    },
+    }
   ];
 
-  constructor(private auth : AuthService) {
+  constructor(private auth : AuthService,  private router : Router) {
   }
 
   ngOnInit() {
+  }
+
+  logout() {
+    // to write logic for logout
+    this.router.navigate(['items']);
   }
 
 }
