23504e61b2204ae6fece9a294bd208c90beff371
lucky4ever4 <cristyapostol93@gmail.com>
2018-10-26 16:53:22 +0300

    get is working

--- src/app/components/star-review-component/star-review.component.ts
+++ src/app/components/star-review-component/star-review.component.ts
@@ -1,8 +1,5 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
-import {StarService} from '@services/star-service/star.service';
-import {Observable} from 'rxjs/Observable';
 import {ActivatedRoute, Router} from '@angular/router';
-import {DomSanitizer} from '@angular/platform-browser';
 import {AuthService} from '../../auth/auth.service';
 import {ItemService} from '@services/item-service/item.service';
 
@@ -16,24 +13,15 @@ export class StarReviewComponent implements OnInit {
 
   hovered = 0;
   readonly = false;
-
   rating: Number = 0;
   itemId: Number;
   userId: Number;
-  // updateRate: EventEmitter<Number> = new EventEmitter();
-
 
   constructor(private itemService: ItemService,
               private route: ActivatedRoute,
               private auth: AuthService) {
 
   }
-
-  // createRating(value: Number) {
-  //   this.rating = value;
-  //   this.updateRate.next(value);
-  //
-  // }
 
   onChange(event) {
     this.itemService.createRating(this.userId, this.itemId, this.rating).subscribe(itemDto => {
